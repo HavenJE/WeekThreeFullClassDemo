216d2c83e29b9740ea3cd8719cb5d70681e961c3
Haven <haven.eldebaque@gmail.com>
2023-06-15 19:24:26 +1000

    Add project files.

---
+++ WeekThreeFullClassDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿// 3.9 Overloading operators
+
+// Watch the following video for a complete program example to create a user-defined class with all important components
+// including data fields, properties, constructors, methods in class and how to write an application that uses the
+// user-defined class.
+
+using WeekThreeFullClassDemo;
+
+class Program
+{
+    static void Main()
+    {
+        // create student object (name of class, name of object is lowercase) 
+        StudentApplicationDemo student1 = new StudentApplicationDemo();
+        // to check the student attributes values
+        Console.WriteLine("Student ID is: {0}, and name is: {1}. Course name is {2}", student1.StudentID, student1.StudentName, student1.CourseName );
+        // The above line will display "Student ID is: 0, and name is: . Course name is" because we have not assign the values for this data field
+
+        // To assign the values for the data fields, we do: 
+        student1.StudentID = 123;
+        student1.CourseName = "Haven";
+        student1.CourseName = "Object oriented programming";
+
+        // To use constructor to create object 
+        StudentApplicationDemo student2 = new StudentApplicationDemo(111, "Lisa", "Business design & Analysis");
+        StudentApplicationDemo student3 = new StudentApplicationDemo(222, "Sam", " "); // even when courseName is left empty, it will be filled by (courseName = "Data Analysis";) that has been set in the StudentApplicationDemo constructor.
+        // Console.WriteLine("Student1 ID is: {0}, and name is: {1}. Course name is {2}", student1.StudentID, student1.StudentName, student1.CourseName);
+        // Console.WriteLine("Student2 ID is: {0}, and name is: {1}. Course name is {2}", student2.StudentID, student2.StudentName, student2.CourseName);
+        // Console.WriteLine("Student3 ID is: {0}, and name is: {1}. Course name is {2}", student3.StudentID, student3.StudentName, student3.CourseName);
+
+        Console.WriteLine("Student 1 information:");
+        student1.DisplayInformation();
+
+        Console.WriteLine("Student 2 information:");
+        student2.DisplayInformation();
+
+        Console.WriteLine("Student 3 information:");
+        student3.DisplayInformation();
+    }
+}
---
+++ WeekThreeFullClassDemo/StudentApplicationDemo.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeekThreeFullClassDemo
+{
+    internal class StudentApplicationDemo
+    {
+        // Data fields or attributes
+        private int studentID;
+        private string studentName;
+        private string courseName;
+
+        // Properties 
+        public int StudentID
+        {
+            get { return studentID; }
+            set { studentID = value; }
+        }
+
+        public string StudentName
+        {
+            get { return courseName; }
+            set { courseName = value; }
+        }
+
+        public string CourseName
+        { 
+            get { return courseName; }
+            set { if (value.Length > 30) 
+                    Console.WriteLine("Course name is too long!");
+                else
+                courseName = value;
+                }
+        }
+
+        // Constructor - a name to create object, starting with public + name of class 
+        public StudentApplicationDemo()
+        {
+            // this constructor is to eliminate the error on the constructor StudentApplicationDemo made over Program.cs
+        }
+        public StudentApplicationDemo(int id, string name, string course)
+        {
+            // Inside constructor body, we assign the attributes to the data types 
+            studentID = id;
+            studentName = name;
+            courseName = "Data Analysis";
+        }
+
+        // Methods
+        // We create methods to display information of the object 
+        public void DisplayInformation()
+        {
+            Console.WriteLine("Student ID is: {0}, Student name is: {1}, Course name is: {2}", studentID, StudentName, CourseName);
+        }
+    }
+}
